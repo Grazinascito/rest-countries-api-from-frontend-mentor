fdf148913c32b40ee64cc7e7b9f79c7c1d70de50
Grazi <grazi.social84@gmail.com>
2021-10-11 14:01:29 -0300

    ajustes finais

--- src/App.js
+++ src/App.js
@@ -12,21 +12,27 @@ function App() {
     const [countries, setCountries] = useState([]);
     const [select, setSelect] = useState('');
     const [filterSelect, setFilterSelect] = useState([]);
+    const [input, setInput] = useState('');
 
     useEffect(() => {
         const fetchData = async () => {
             const response = await fetch(url);
             const countries = await response.json();
             setCountries(countries);
+            setFilterSelect(countries);
         }
         fetchData();
     }, []);
 
     const handleFilter = (event) => {
-        if(event.key !== 'Enter') return;
+        if(event.key !== 'Enter'){
+            setInput(event.target.value)
+            return 
+        }
+
         event.preventDefault();
 
-       const filterCountry = countries.filter(country => country.name.common === event.target.value);
+       const filterCountry = filterSelect.filter(country => country.name.common.toLowerCase().includes(event.target.value.toLowerCase()));
 
        setFilterCountry(filterCountry);
     }
@@ -38,20 +44,32 @@ function App() {
       const filterSelect = countries.filter(select => select.region === event.target.value);
 
       setFilterSelect(filterSelect);
+
+      if(event.target.value === ''){
+        setFilterSelect(countries);
+      }
+
     }
 
-    console.log(filterCountry)
+    useEffect(() => {
+
+        if(countries){
+            const filterCountry = filterSelect.filter(country => country.name.common.toLowerCase().includes(input.toLowerCase()));
+
+            setFilterCountry(filterCountry);
+        }
+    }, [filterSelect]);
 
   return (
     <>
       <Header />
       <Filter
-      handleFilter={handleFilter}
+        handleFilter={handleFilter}
       />
     <section className="filter">
         <div className="region-filter">
             <select value={select} onChange={handleSelect}  name="select" id="select" className="select">
-                <option value="Filter by Region">Filter by Region</option>
+                <option value="">Filter by Region</option>
                 <option value="Africa">Africa</option>
                 <option value="Americas">Americas</option>
                 <option value="Asia">Asia</option>
@@ -62,7 +80,7 @@ function App() {
      </section>
 
       {
-      filterCountry.length > 0 ?
+      filterCountry.length > 0 &&
       <section className="grid">
       {filterCountry.map((country) => {
           const {cca2, name, region, capital, flags} = country;
@@ -81,26 +99,8 @@ function App() {
           );
       })} 
   </section>
-      :
-      <section className="grid">
-      {countries.map((country) => {
-          const {cca2, name, region, capital, flags} = country;
-          return (
-              <article key={cca2}>
-                  <div>
-                      <img src={flags[0]} alt={name.common} />
-                      <div className="details">
-                          <h3>{name.common}</h3>
-                          <h4>official name: <span>{name.official}</span></h4>
-                          <h4>region: <span>{region}</span></h4>
-                          <h4>capital: <span>{capital}</span></h4> 
-                      </div>                        
-                  </div>
-              </article>
-          );
-      })} 
-  </section>
-    } 
+    }
+    
       
     </>
   );
